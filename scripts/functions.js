f0a840f5937b06ae7d11c4cd388325c787fea5bf
Pangolecimal <58990743+Pangolecimal@users.noreply.github.com>
2023-06-03 10:38:54 +0300

    smth ✨✨✨
    added a lot of jsdoc and setter/getter for the board state

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,13 +1,13 @@
 //#region //* Setup & Event Listeners
 function setup() {
 	// updateViewportSize()
-	insert_default_cells(document.querySelector('[data-dom="board"]'))
+	insert_default_cells(get_elements.dom(DOM.BOARD))
 	insert_gradients(document.querySelector('.gradients'))
 	reset_data_attributes()
 	set_modal_events()
 	set_settings_events()
 
-	// get_elements.query('.open-modal')[0].click()
+	// get_elements.query('.open-modal').click()
 }
 
 function updateViewportSize() {
@@ -17,26 +17,28 @@ function updateViewportSize() {
 }
 
 function insert_default_cells(board) {
-	for (let i = 0; i < 12; i++) {
-		for (let j = 0; j < 12; j++) {
+	for (let i = 0; i < BOARD_SIZE; i++)
+		for (let j = 0; j < BOARD_SIZE; j++)
 			board.innerHTML += `
-            <div data-dom="cell" data-occupied="false" data-x="${j}" data-y="${i}"
-                data-player="${i < 6 ? PLAYER_TYPE.BLUE : PLAYER_TYPE.GREEN}"
-                data-shade="${i % 2 === j % 2 ? 'light' : 'dark'}">
-                <svg class="elemental" viewbox="0 0 100 100" data-dom="cell_svg">
-                    <path class="elemental-shape" data-dom="cell_path"/>
-                    <circle class="elemental-health-background" cx="50" cy="50" r="45" data-dom="cell_path"/>
-                    <path class="elemental-health-life" data-dom="cell_path"/>
-                    <path class="elemental-health-hit" data-dom="cell_path"/>
-                    <circle class="elemental-health-boundary" cx="50" cy="50" r="45" data-dom="cell_path"/>
-                </svg>
-            </div>`
-		}
-	}
+            <div data-dom="cell" data-occupied="false" 
+                data-x="${j}" data-y="${i}" data-id="${j}-${i}"  
+                data-player="${i < 6 ? PLAYER_TYPE.BLUE : PLAYER_TYPE.GREEN}" 
+                data-shade="${i % 2 === j % 2 ? 'light' : 'dark'}"></div>`
+
+	get_elements.dom(DOM.CELL).forEach((cell) => {
+		cell.innerHTML += `
+        <svg class="elemental" viewbox="0 0 100 100" data-dom="cell_svg">
+            <path class="elemental-shape" data-dom="cell_path"/>
+            <circle class="elemental-health-background" cx="50" cy="50" r="45" data-dom="cell_path"/>
+            <path class="elemental-health-life" data-dom="cell_path"/>
+            <path class="elemental-health-hit" data-dom="cell_path"/>
+            <circle class="elemental-health-boundary" cx="50" cy="50" r="45" data-dom="cell_path"/>
+        </svg>`
+	})
 }
 
 function insert_gradients(gradient_svg) {
-	const elements = Object.values(ELEMENTS)
+	const elements = Object.values(ELEMENT)
 	for (var i = 0, gradients = ''; i < 6; i++) {
 		gradients += `
         <radialgradient id="gradient-${elements[i]}">
@@ -82,13 +84,13 @@ function set_settings_events() {
 				.filter((input) => input.parentElement.parentElement.dataset.player === color)
 				.map((input) => (input.checked ? input.dataset.type.toUpperCase() : ''))
 				.filter((el) => el !== '')
-				.map((e) => ELEMENTS[e])
+				.map((e) => ELEMENT[e])
 
 		const elements = { green: foo(PLAYER_TYPE.GREEN), blue: foo(PLAYER_TYPE.BLUE) }
 
 		clear_cells(cells)
 		generate_cells(cells, elements)
-		get_elements.query('.close-modal')[0].click()
+		get_elements.query('.close-modal').click()
 	})
 }
 
@@ -111,22 +113,32 @@ function reset_data_attributes() {
 //#endregion
 
 //#region //* General
-function insert_elemental({ cell, element, level = 1, health = 1, hit = 0 }) {
+/**
+ * @param {Elemental}
+ */
+function insert_elemental({ cell, element, level = 1, health = 1 }) {
 	cell.dataset.occupied = 'true'
 	cell.dataset.element = element
 	cell.dataset.level = level
 	cell.dataset.health = health
-	cell.dataset.hit = hit
 }
 
-function remove_elemental(cell) {
+/**
+ * @param {Element} cell
+ * @param {boolean} animate
+ */
+function reset_cell_elemental(cell, animate) {
 	cell.dataset.occupied = 'false'
-	cell.dataset.element = null
-	cell.dataset.level = null
+	// cell.dataset.element = null
+	// cell.dataset.level = null
 	cell.dataset.health = null
 	cell.dataset.hit = null
+	cell.dataset.animate = 'true'
 }
 
+/**
+ * @param {PLAYER_TYPE} type
+ */
 function get_cells(type) {
 	const cells_raw = get_elements.dom('cell')
 	if (type === PLAYER_TYPE.BLUE) return cells_raw.slice(0, 72)
@@ -134,79 +146,201 @@ function get_cells(type) {
 	return cells_raw
 }
 
+/**
+ * @param {Array<Element>} cells
+ */
 function clear_cells(cells) {
-	cells.flat().forEach((cell) => remove_elemental(cell))
+	cells.flat().forEach((cell) => reset_cell_elemental(cell))
 }
 
+/**
+ * @param {Array<Element>} elements
+ * @param {boolean} forced_state
+ */
 function toggle_active(elements, forced_state) {
 	;[elements].flat(100).forEach((e) => set_data(e, 'active', forced_state ?? !(e.dataset.active == 'true')))
 }
 
 /**
  ** BOARD: row_0/row_1/row_2/row_3/row_4/row_5/row_6/row_7/row_8/row_9/row_10/row_11/
- ** ROW: Element+Level+Health|Empty: Element=[a|e|f|n|r|w] Level=[1..3]  Health=[1..6] ?Empty:_[1..12]?
- ** Example: a11|11/1|a21|10/2|a21|9/3|a21|8/4|a21|7/5|a21|6/6|a21|5/7|a21|4/8|a21|3/9|a21|2/10|a21|1/11|a21
+ ** ROW: Element+Level+Health|Empty: Element=[a|e|f|n|r|w] Level=[1..3]  Health=[1..6] ?Empty:_[1..BOARD_SIZE]?
+ ** Debug: 6|a31|r31|f31|w31|n31|e31/a11|r11|f11|w11|n11|e11|a32|r32|f32|w32|n32|e32/6|a33|r33|f33|w33|n33|e33/a21|r21|f21|w21|n21|e21|a34|r34|f34|w34|n34|e34/a22|r22|f22|w22|n22|e22|a35|r35|f35|w35|n35|e35/6|a36|r36|f36|w36|n36|e36/6|a31|r31|f31|w31|n31|e31/a11|r11|f11|w11|n11|e11|a32|r32|f32|w32|n32|e32/6|a33|r33|f33|w33|n33|e33/a21|r21|f21|w21|n21|e21|a34|r34|f34|w34|n34|e34/a22|r22|f22|w22|n22|e22|a35|r35|f35|w35|n35|e35/6|a36|r36|f36|w36|n36|e36
+ * @param {Board} board
+ * @returns {string}
  */
 function get_board_state(board) {
 	let state = ''
-	for (let j = 0; j < 12; j++) {
+	for (let j = 0; j < BOARD_SIZE; j++) {
 		let empty_count = 0
-		for (let i = 0; i < 12; i++) {
-			const cell = board.cells[i + j * 12]
+		for (let i = 0; i < BOARD_SIZE; i++) {
+			const cell = board.cells[i + j * BOARD_SIZE]
 			const elemental = board.find_elemental(cell)
 
-			if (elemental != null) {
-				if (empty_count > 0) {
-					state += `${empty_count}|`
-					empty_count = 0
-				}
-				state += `${elemental.element[0]}${elemental.level}${elemental.health}|` // `${elemental.element[0]}${i.toString(16)}${j.toString(16)}|`
-			}
+			if (elemental != null && empty_count > 0) empty_count = 0 + (0 === (state += empty_count.toString(16) + '|')) //? update state & set empty_count to 0
+			if (elemental != null) state += `${elemental.element[0]}${elemental.level}${elemental.health}|`
 			if (elemental == null) empty_count++
 		}
 		if (empty_count > 0) state += `${empty_count}`
 		if (state.at(-1) === '|') state = state.slice(0, -1) //? removes last '|' from state
-		state += '/'
+		if (j < BOARD_SIZE - 1) state += '/'
 	}
 
 	return state
 }
+// get_board_state.RANDOM = () => {
+// 	const num = 30 + random.sign() * random.int(4)
+// 	const elementals = []
+// 	for (let i = 0; i < num; i++) {
+// 		const elemental = random.array(Object.values(ELEMENT))
+// 		elementals.push()
+// 	}
+// }
 
-function set_board_state(board, board_state) {}
+/**
+ * @param {Board} board
+ * @param {string} state
+ */
+function set_board_state(board, state) {
+	board.remove_elementals()
+	const rows = state.split('/').map((row) => row.split('|'))
+	rows.length = BOARD_SIZE
 
+	rows.forEach((row, y) => {
+		let x = 0
+		row.forEach((data) => {
+			if (data.length === 3) {
+				const cell = board.cells[x + y * BOARD_SIZE]
+
+				const element = Object.values(ELEMENT).find((e) => e[0] === data[0])
+				const level = parseInt(data[1])
+				const health = parseInt(data[2])
+
+				const elemental = new Elemental(element, level)
+
+				board.set_elemental(cell, elemental)
+			}
+			x += data.length === 1 ? parseInt(data, 16) : 1
+		})
+	})
+
+	board.insert_elementals()
+}
+set_board_state.DEBUG = () => {
+	set_board_state(
+		game.board,
+		'6|a31|r31|f31|w31|n31|e31/a11|r11|f11|w11|n11|e11|a32|r32|f32|w32|n32|e32/6|a33|r33|f33|w33|n33|e33/a21|r21|f21|w21|n21|e21|a34|r34|f34|w34|n34|e34/a22|r22|f22|w22|n22|e22|a35|r35|f35|w35|n35|e35/6|a36|r36|f36|w36|n36|e36/6|a31|r31|f31|w31|n31|e31/a11|r11|f11|w11|n11|e11|a32|r32|f32|w32|n32|e32/6|a33|r33|f33|w33|n33|e33/a21|r21|f21|w21|n21|e21|a34|r34|f34|w34|n34|e34/a22|r22|f22|w22|n22|e22|a35|r35|f35|w35|n35|e35/6|a36|r36|f36|w36|n36|e36',
+	)
+}
+
+/**
+ * @param {Board} board
+ * @param {string} state
+ * @returns {string}
+ */
 function prettify_board_state(board_state) {
-	let state_log = '-----+'.repeat(12) + '\n ' + board_state.replaceAll('/', `|\n${'-----+'.repeat(12)}\n `).replaceAll('|', ' | ')
-	for (let i = 1; i <= 12; i++) state_log = state_log.replaceAll(` ${i} `, '     |'.repeat(i).slice(0, -1))
-	state_log = state_log.substring(0, 882) + '=====+'.repeat(12) + state_log.substring(953 + 1)
-	return state_log
-		.split('\n')
-		.map((s, i) => (i % 2 === 0 ? '+' : '|') + s.trimEnd())
-		.join('\n')
-		.slice(0, -1)
+	const [cw, ch] = [6, 2] //? log_cell_width, log_cell_height
+	const [bw, bh] = [cw * BOARD_SIZE + 1, ch * BOARD_SIZE + 1] //? log_board_width, log_board_height
+	const log_board = new Array(bh).fill().map(() => new Array(bw).fill(' '))
+
+	for (let y = 0; y < bh; y++) {
+		for (let x = 0; x < bw; x++) {
+			if (x % cw === 0 && y % ch === 0) log_board[y][x] = '┼'
+			if (x === 000000 && y % ch === 0) log_board[y][x] = '├'
+			if (x === bw - 1 && y % ch === 0) log_board[y][x] = '┤'
+			if (x % cw === 0 && y === 000000) log_board[y][x] = '┬'
+			if (x % cw === 0 && y === bh - 1) log_board[y][x] = '┴'
+			if (x === 000000 && y === 000000) log_board[y][x] = '┌'
+			if (x === 000000 && y === bh - 1) log_board[y][x] = '└'
+			if (x === bw - 1 && y === bh - 1) log_board[y][x] = '┘'
+			if (x === bw - 1 && y === 000000) log_board[y][x] = '┐'
+
+			if (x % cw !== 0 && y % ch === 00000000000000000) log_board[y][x] = '─'
+			if (x % cw !== 0 && (y + 1) % (cw * 2 + 1) === 0) log_board[y][x] = '━'
+			if (x % cw === 0 && (y + 1) % (cw * 2 + 1) === 0) log_board[y][x] = '┿'
+			if (x === bw - 1 && (y + 1) % (cw * 2 + 1) === 0) log_board[y][x] = '┥'
+			if (x === 000000 && (y + 1) % (cw * 2 + 1) === 0) log_board[y][x] = '┝'
+
+			if (x % cw === 0 && y % ch !== 0) log_board[y][x] = '│'
+		}
+	}
+
+	const log_cells = board_state.split('/').map((row) => row.split('|'))
+	log_cells.forEach((row, y) => {
+		let x = 0
+		row.forEach((cell) => {
+			if (cell.length === 3) {
+				log_board[y * ch + 1][x * cw + 2 + 0] = cell[0]
+				log_board[y * ch + 1][x * cw + 2 + 1] = cell[1]
+				log_board[y * ch + 1][x * cw + 2 + 2] = cell[2]
+			}
+			x += cell.length === 1 ? parseInt(cell) : 1
+		})
+	})
+
+	return log_board.map((row) => row.join('')).join('\n')
 }
 //#endregion
 
 //#region //* Helpers
 const get_elements = {
-	dom: (query) => Array.from(document.querySelectorAll(`[data-dom="${query}"]`)),
-	dom_active: (query) => Array.from(document.querySelectorAll(`[data-active="true"][data-dom="${query}"]`)),
-	dom_player: (query, player) => Array.from(document.querySelectorAll(`[data-player="${player}"][data-dom="${query}"]`)),
-	data: (data, query) => Array.from(document.querySelectorAll(`[data-${data}="${query}"]`)),
-	query: (query) => Array.from(document.querySelectorAll(`${query}`)),
+	/**
+	 * @param {string} query
+	 * @returns {(Element|Array<Element>)}
+	 */
+	dom: (query) => get_elements._helper(Array.from(document.querySelectorAll(`[data-dom="${query}"]`))),
+
+	/**
+	 * @param {string} query
+	 * @param {PLAYER_TYPE} player
+	 * @returns {(Element|Array<Element>)}
+	 */
+	dom_player: (query, player) => get_elements._helper(Array.from(document.querySelectorAll(`[data-player="${player}"][data-dom="${query}"]`))),
+
+	/**
+	 * query: `dom=DOM data-TYPE=VALUE`
+	 * @param {string} dom
+	 * @param {string} type
+	 * @param {string} value
+	 * @returns {(Element|Array<Element>)}
+	 */
+	data: (dom, type, value) => get_elements._helper(Array.from(document.querySelectorAll(`[data-${type}="${value}"][data-dom="${dom}"]`))),
+
+	/**
+	 * @param {string} query
+	 * @returns {(Element|Array<Element>)}
+	 */
+	query: (query) => get_elements._helper(Array.from(document.querySelectorAll(`${query}`))),
+
+	/**
+	 * @param {Element} element
+	 * @param {DOM} dom
+	 * @returns {(Element|Array<Element>)}
+	 */
 	closest_dom: (element, dom) => element.closest(`[data-dom="${dom}"]`),
+
+	/**
+	 * @param {(Element|Array<Element>)} result
+	 * @returns {(Element|Array<Element>)}
+	 */
+	_helper: (result) => (result.length === 1 ? result[0] : result),
 }
 
+/**
+ * @param {Element} element
+ * @returns {string}
+ */
 function get_data(element) {
 	if (element == null) return console.error('Invalid element', element)
 	return JSON.parse(JSON.stringify(element?.dataset))
 }
 
+/**
+ * @param {Element} element
+ * @param {string} type
+ * @param {string} data
+ */
 function set_data(element, type, data) {
 	element.dataset[type] = data
-}
-
-function allEqualNotNull(array) {
-	return array.every((v) => v === array[0] && v != null && v !== 'null')
 }
 
 ;(function (global) {
@@ -221,11 +355,15 @@ function allEqualNotNull(array) {
 		return Math.floor(rng.float(a, b))
 	}
 	rng.sign = function () {
-		return rng.array([1, -1])
+		return Math.round(Math.random()) * 2 - 1
 	}
 	rng.array = function (a) {
-		if (!Array.isArray(a)) return console.error('random function error: ', arguments)
+		if (a == null || !Array.isArray(a)) return console.error('random function error: ', arguments)
 		return a[rng.int(a.length)]
+	}
+	rng.object = function (o) {
+		if (o == null || typeof o !== 'object') return console.error('random function error: ', arguments)
+		return rng.array(Object.values(o))
 	}
 	rng.weight = function (a) {
 		// a = [['a', 1], ['b', 2], ['c', 3]]; <-> a: [[value, weight]; N]
--- scripts/objects.js
+++ scripts/objects.js
@@ -1,12 +1,16 @@
-const ELEMENTS = {
+const BOARD_SIZE = 12
+
+/** @enum {string} */
+const ELEMENT = {
 	AIR: 'air',
-	ENERGY: 'energy',
-	FIRE: 'fire',
-	NATURE: 'nature',
 	ROCK: 'rock',
+	FIRE: 'fire',
 	WATER: 'water',
+	NATURE: 'nature',
+	ENERGY: 'energy',
 }
 
+/** @enum {string} */
 const ACTIONS = {
 	ATTACK: 'attack',
 	MOVE: 'move',
@@ -14,6 +18,7 @@ const ACTIONS = {
 	CONFIRM: 'confirm',
 }
 
+/** @enum {string} */
 const SPELLS = {
 	FOREST_MASTER_STAFF: 'forest-master-staff',
 	THE_LADY_OF_THE_LAKE_VIAL: 'the-lady-of-the-lake-vial',
@@ -21,12 +26,35 @@ const SPELLS = {
 	PENDULUM_OF_REVERSED_TIME: 'pendulum-of-reversed-time',
 	METEOR_SHOWER: 'meteor-shower',
 }
+/** @enum {number} */
+const LEVEL = {
+	0: 1,
+	1: 2,
+	2: 3,
+}
 
-const LEVELS = [1, 2, 3]
-const MAX_HEALTH = [1, 2, 6]
-const DAMAGE = [1, 2, 4]
-const REACH = [3, 5, 7]
+/** @enum {number} */
+const MAX_HEALTH = {
+	0: 1,
+	1: 2,
+	2: 6,
+}
 
+/** @enum {number} */
+const DAMAGE = {
+	0: 1,
+	1: 2,
+	2: 4,
+}
+
+/** @enum {number} */
+const REACH = {
+	0: 3,
+	1: 5,
+	2: 7,
+}
+
+/** @enum {number} */
 const SPELL_MAX_CHARGE = {
 	FOREST_MASTER_STAFF: 4,
 	THE_LADY_OF_THE_LAKE_VIAL: 5,
@@ -35,22 +63,34 @@ const SPELL_MAX_CHARGE = {
 	METEOR_SHOWER: 10,
 }
 
+/** @enum {number} */
 const SPELL_DAMAGE = {
 	THORN_VINES: 2,
 	PYRAMID: 1,
 	METEOR_SHOWER: 4,
 }
 
+/** @enum {string} */
 const PLAYER_TYPE = {
 	GREEN: 'green',
 	BLUE: 'blue',
 }
 
+/** @enum {string} */
 const GAME_STATE = {
 	STARTED: 'started',
 	WAITING: 'waiting', //? PLAYER INPUT
 	RUNNING: 'running', //? GAME ANIMATIONS
 	FINISHED: 'finished',
+}
+
+/** @enum {string} */
+const DOM = {
+	BOARD: 'board',
+	CONTROLLER: 'controller',
+	CELL: 'cell',
+	SPELL: 'spell',
+	ACTION: 'action',
 }
 
 /** //* additional rules:
@@ -105,10 +145,10 @@ class Game {
 				if (has_moved) return (has_moved = false)
 
 				let closest_element //? cell | spell | action
-				if (type === 'board') closest_element = get_elements.closest_dom(e.target, 'cell')
-				if (type === 'controller') closest_element = get_elements.closest_dom(e.target, 'spell') ?? get_elements.closest_dom(e.target, 'action')
+				if (type === DOM.BOARD) closest_element = get_elements.closest_dom(e.target, DOM.CELL)
+				if (type === DOM.CONTROLLER) closest_element = get_elements.closest_dom(e.target, DOM.SPELL) ?? get_elements.closest_dom(e.target, DOM.ACTION)
 
-				if (type === 'controller' && get_data(get_elements.closest_dom(e.target, 'controller')).active === 'false') return
+				if (type === DOM.CONTROLLER && get_data(get_elements.closest_dom(e.target, DOM.CONTROLLER)).active === 'false') return
 				if (closest_element == null) return
 
 				CLICK_LOG.history.push(CLICK_LOG.last)
@@ -116,23 +156,23 @@ class Game {
 			})
 		}
 
-		fn(this.board.self, 'board')
-		fn(this.controllers.blue.self, 'controller')
-		fn(this.controllers.green.self, 'controller')
+		fn(this.board.self, DOM.BOARD)
+		fn(this.controllers.blue.self, DOM.CONTROLLER)
+		fn(this.controllers.green.self, DOM.CONTROLLER)
 	}
 
 	game_loop() {
 		if (this.state === GAME_STATE.STARTED && (this.state = GAME_STATE.WAITING)) {
-			console.log('GAME STARTED')
+			// console.log('GAME STARTED')
 
 			this.board.insert_elementals()
 			this.set_event_listeners()
 		}
 
 		if (this.state === GAME_STATE.WAITING && (this.state = GAME_STATE.RUNNING)) {
-			console.log('GAME WAITING')
+			// console.log('GAME WAITING')
 		} else if (this.state === GAME_STATE.RUNNING && (this.state = GAME_STATE.WAITING)) {
-			console.log('GAME RUNNING')
+			// console.log('GAME RUNNING')
 
 			if (CLICK_LOG.last != null && get_data(CLICK_LOG.last).type === 'skip') {
 				this.controllers.blue.toggle_active()
@@ -145,18 +185,24 @@ class Game {
 
 		if (this.state === GAME_STATE.FINISHED) {
 			console.log("GAME OVER - how in the Lord's name did you get here?")
-		} //else setTimeout(() => this.game_loop(), 100)
+			alert("GAME OVER - how in the Lord's name did you get here?")
+		} else setTimeout(() => this.game_loop(), 1)
 	}
 }
 
 class Controller {
 	constructor(player) {
 		this.color = player
-		this.self = get_elements.dom_player('controller', this.color)[0]
-		this.spells = get_elements.dom_player('spell', this.color)
-		this.actions = get_elements.dom_player('action', this.color)
+		this.self = get_elements.dom_player(DOM.CONTROLLER, this.color)
+		this.actions = get_elements.dom_player(DOM.ACTION, this.color)
+		this.spells = get_elements.dom_player(DOM.SPELL, this.color)
+		this.charges = Object.keys(SPELLS)
 	}
 
+	/**
+	 * @param {boolean} forced_state
+	 * @returns {Board}
+	 */
 	toggle_active(forced_state) {
 		toggle_active(this.self, forced_state)
 		return this
@@ -165,84 +211,141 @@ class Controller {
 
 class Board {
 	static default() {
-		return new Board(get_cells().flat(), {
-			green: [ELEMENTS.WATER, ELEMENTS.AIR],
-			blue: [ELEMENTS.NATURE, ELEMENTS.ROCK],
+		return new Board(get_cells(), {
+			green: [ELEMENT.WATER, ELEMENT.AIR],
+			blue: [ELEMENT.NATURE, ELEMENT.ROCK],
 		})
 	}
 
+	/**
+	 * @param {Array<HTMLElement>} cells
+	 * @param {Array<ELEMENT>} elements
+	 * @returns {Board}
+	 */
 	constructor(cells, elements) {
 		this.cells = cells
-		this.self = get_elements.dom('board')[0]
+		this.self = get_elements.dom(DOM.BOARD)
 		this.elements = elements
 		this.elementals = this.generate_elementals()
 	}
 
-	set_elements(elements) {
-		this.elements = elements
-	}
-
+	/**
+	 * @returns {Array<Elemental>}
+	 */
 	generate_elementals() {
 		const elementals = []
-		const num = 30 - random.int(4, 6)
-
+		const num = 30 + random.sign() * random.int(4)
 		for (let i = 0; i < num; i++) {
 			Object.values(PLAYER_TYPE).forEach((player) => {
 				if (this.elements[player].length === 0) return
-				const cell = random.array(this.cells.flat().filter((c) => get_data(c).occupied === 'false' && get_data(c).player === player))
+				const cell = random.array(this.cells.filter((c) => get_data(c).occupied === 'false' && get_data(c).player === player))
 				elementals.push(new Elemental(random.array(this.elements[player]), 1).bind(cell))
 			})
 		}
-
 		return elementals
 	}
 
+	/**
+	 * @param {ELEMENT} elements
+	 * @returns {Board}
+	 */
+	set_elements(elements) {
+		this.elements = elements
+		return this
+	}
+
+	/**
+	 * @returns {Board}
+	 */
 	insert_elementals() {
 		this.elementals.forEach((e) => insert_elemental(e.data))
+		return this
 	}
 
-	remove_elementals() {
-		this.cells.forEach(remove_elemental)
-	}
-
+	/**
+	 * @param {HTMLElement} cell
+	 * @returns {Elemental}
+	 */
 	find_elemental(cell) {
-		return this.elementals.find((e) => get_data(e.cell).x === get_data(cell).x && get_data(e.cell).y === get_data(cell).y)
+		return this.elementals.find((e) => e.pos.x === get_data(cell).x && e.pos.y === get_data(cell).y)
 	}
 
+	/**
+	 * @param {HTMLElement} cell
+	 * @param {Elemental} elemental
+	 * @returns {Board}
+	 */
+	set_elemental(cell, elemental) {
+		this.elementals.push(elemental.bind(cell))
+		return this
+	}
+
+	/**
+	 * @returns {Board}
+	 */
+	remove_elementals() {
+		this.cells.forEach((cell) => {
+			this.remove_elemental(cell)
+		})
+		return this
+	}
+
+	/**
+	 * @param {HTMLElement} cell
+	 * @param {Elemental} elemental
+	 * @returns {Board}
+	 */
+	remove_elemental(cell, elemental) {
+		const elemental_ = elemental ?? this.find_elemental(cell)
+		this.elementals.splice(this.elementals.indexOf(elemental_), 1)
+		reset_cell_elemental(cell, true)
+		return this
+	}
+
+	/**
+	 * @param {PLAYER_TYPE} player
+	 * @returns {Board}
+	 */
 	merge_cells(player) {
 		if (player !== PLAYER_TYPE.BLUE && player !== PLAYER_TYPE.GREEN) return console.error('invalid player: ', player)
 		const cells = get_cells(player)
-		const [y1, y2] = [0, 4]
-		const [x1, x2] = [0, 10]
-		const cells_to_remove = new Set()
-		const cells_to_upgrade = new Set()
+		const [y1, y2] = [0, 3]
+		const [x1, x2] = [0, 9]
+		const id_to_remove = new Set()
+		const id_to_upgrade = new Set()
 
-		for (let y = y1; y < y2; y++) {
-			for (let x = x1; x < x2; x++) {
+		for (let y = y1; y <= y2; y++) {
+			for (let x = x1; x <= x2; x++) {
 				const check_area = []
-				for (let j = y; j < y + 3; j++) for (let i = x; i < x + 3; i++) check_area.push(cells[i + j * 12])
+				for (let j = y; j < y + 3; j++) for (let i = x; i < x + 3; i++) check_area.push(cells[i + j * BOARD_SIZE])
 
 				const result = this.check_merge(check_area)
 				if (result.length === 0) continue
-				// console.log({ area: { x, y }, check_area, result })
-				// console.log(result.map((r) => check_area[r[1]]))
 
 				result.forEach((config) => {
-					cells_to_remove.add(check_area[config[0]])
-					cells_to_upgrade.add(check_area[config[1]])
-					cells_to_remove.add(check_area[config[2]])
+					id_to_remove.add(get_data(check_area[config[0]]).id)
+					id_to_upgrade.add(get_data(check_area[config[1]]).id)
+					id_to_remove.add(get_data(check_area[config[2]]).id)
 				})
 			}
 		}
 
-		console.log({ cells_to_remove, cells_to_upgrade })
+		for (const id of id_to_upgrade) id_to_remove.delete(id)
 
-		// cells_to_remove.forEach((pos) => remove_elemental(new_cells[pos.y][pos.x]))
+		console.log({ id_to_remove, id_to_upgrade })
+
+		for (const id of id_to_remove) this.remove_elemental(get_elements.data(DOM.CELL, 'id', id))
+		for (const id of id_to_upgrade) this.find_elemental(get_elements.data(DOM.CELL, 'id', id)).upgrade()
+
+		return this
 	}
 
+	/**
+	 * @param {Array<HTMLElement>} cells
+	 * @returns {Array<number>}
+	 */
 	check_merge(cells) {
 		const elementals = cells.map((cell) => this.find_elemental(cell))
-
 		const elements = elementals.map((e) => e?.element)
 		const levels = elementals.map((e) => e?.level)
 		const configurations = [
@@ -255,34 +358,48 @@ class Board {
 			[0, 4, 8],
 			[2, 4, 6],
 		]
+
 		const result = configurations.filter((config) => {
 			const config_elements = config.map((i) => elements[i])
 			const config_levels = config.map((i) => levels[i])
-			return allEqualNotNull(config_elements) && allEqualNotNull(config_levels)
+
+			const elements_ok = config_elements.every((e) => e === config_elements[0] && e != null)
+			const levels_ok = config_levels.every((l) => l !== LEVEL[2] && l === config_levels[0] && l != null)
+
+			return elements_ok && levels_ok
 		})
+
 		return result
 	}
 }
 
 class Elemental {
+	/**
+	 * @param {ELEMENT} default_element
+	 * @returns {Elemental}
+	 */
 	static random(default_element = null) {
-		const element = default_element ?? random(Object.values(ELEMENTS))
+		const element = default_element ?? random(Object.values(ELEMENT))
 		const level = random.weight([
-			[LEVELS[0], 3],
-			[LEVELS[1], 2],
-			[LEVELS[2], 1],
+			[LEVEL[0], 3],
+			[LEVEL[1], 2],
+			[LEVEL[2], 1],
 		])
 		return new Elemental(element, level)
 	}
 
-	constructor(element, level, health) {
+	/**
+	 * @param {ELEMENT} element
+	 * @param {LEVEL} level
+	 * @returns {Elemental}
+	 */
+	constructor(element, level) {
 		this.element = element
-		this.level = level
-		this.health = health ?? MAX_HEALTH[level - 1]
+		this.level = parseInt(level)
 
-		this.hit = 0
-		this.damage = DAMAGE[level - 1]
-		this.reach = REACH[level - 1]
+		this.health = MAX_HEALTH[this.level - 1]
+		this.damage = DAMAGE[this.level - 1]
+		this.reach = REACH[this.level - 1]
 
 		this.cell = null
 		this.pos = { x: null, y: null }
@@ -290,6 +407,10 @@ class Elemental {
 		return this
 	}
 
+	/**
+	 * @param {HTMLElement} cell
+	 * @returns {Elemental}
+	 */
 	bind(cell) {
 		set_data(cell, 'occupied', true)
 		this.cell = cell
@@ -298,17 +419,35 @@ class Elemental {
 		return this
 	}
 
+	/**
+	 * @returns {Elemental}
+	 */
 	upgrade() {
-		return new Elemental(this.element, Math.min(this.level + 1, LEVELS.at(-1)))
+		this.level = Math.min(this.level + 1, LEVEL[2])
+		this.damage = DAMAGE[this.level - 1]
+		this.health = MAX_HEALTH[this.level - 1]
+		this.update_cell()
+		return this
 	}
 
+	/**
+	 * @returns {Elemental}
+	 */
+	update_cell() {
+		reset_cell_elemental(this.cell)
+		insert_elemental(this.data)
+		return this
+	}
+
+	/**
+	 * @returns {Object<string, string>} `[[data]]` - all properties
+	 */
 	get data() {
 		return {
 			cell: this.cell,
 			element: this.element,
 			level: this.level,
 			health: this.health,
-			hit: this.hit,
 			damage: this.damage,
 			reach: this.reach,
 		}
